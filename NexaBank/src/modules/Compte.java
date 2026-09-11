8160fc85b7dc896102423c6776836185d37f4b64
Salma GTU <salmajaddar7@gmail.com>
2026-09-11 11:39:03 +0100

    feat(model): define bank account domain entity

--- NexaBank/src/modules/Compte.java
+++ NexaBank/src/modules/Compte.java
@@ -1,41 +1,41 @@
-//package modules;
-//import java.util.HashSet;
-//import java.util.Set;
-//import Enums.TypeCompte;
-//public class Compte {
-//    private String numcomp;
-//    private Double solde;
-//    private TypeCompte typeCompte;
-//    private Set<Transaction> historiqueTransactions ;
-//
-//    public Compte(String numcomp,Double solde,TypeCompte typeCompte){
-//        this.numcomp=numcomp;
-//        this.solde=solde;
-//        this.typeCompte=typeCompte;
-//        this.historiqueTransactions =new HashSet<Transaction>();
-//    }
-//
-//    public Set<Transaction> getHistoriqueTransactions() {
-//        return historiqueTransactions;
-//    }
-//
-//    public String getNumcomp() {
-//        return numcomp;
-//    }
-//
-//    public Double getSolde() {
-//        return solde;
-//    }
-//
-//    public TypeCompte getTypeCompte() {
-//        return typeCompte;
-//    }
-//
-//    public void setSolde(Double s){
-//        this.solde=s;
-//    }
-//
-//    public void setTypeCompte(TypeCompte type){
-//        this.typeCompte=type;
-//    }
-//}
+package modules;
+import java.util.HashSet;
+import java.util.Set;
+import Enums.TypeCompte;
+public class Compte {
+    private String numcomp;
+    private Double solde;
+    private TypeCompte typeCompte;
+    private Set<Transaction> historiqueTransactions ;
+
+    public Compte(String numcomp,Double solde,TypeCompte typeCompte){
+        this.numcomp=numcomp;
+        this.solde=solde;
+        this.typeCompte=typeCompte;
+        this.historiqueTransactions =new HashSet<Transaction>();
+    }
+
+    public Set<Transaction> getHistoriqueTransactions() {
+        return historiqueTransactions;
+    }
+
+    public String getNumcomp() {
+        return numcomp;
+    }
+
+    public Double getSolde() {
+        return solde;
+    }
+
+    public TypeCompte getTypeCompte() {
+        return typeCompte;
+    }
+
+    public void setSolde(Double s){
+        this.solde=s;
+    }
+
+    public void setTypeCompte(TypeCompte type){
+        this.typeCompte=type;
+    }
+}
