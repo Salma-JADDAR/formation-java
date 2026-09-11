9b0e23473bd639f031680a2ef8294f990f34823d
Salma GTU <salmajaddar7@gmail.com>
2026-09-11 12:26:48 +0100

    feat(service): implement transaction deletion with existence validation

--- NexaBank/src/Service/CompteService.java
+++ NexaBank/src/Service/CompteService.java
@@ -25,17 +25,17 @@ public class CompteService {
         }
     }
 
-//    public boolean supprimerTransaction(Transaction t, Compte c){
-//        if (deja(c,t)){
-//            c.getHistoriqueTransactions().remove(t);
-//            System.out.println("tu as supprimer le  transacrtion"+t.getIdTransaction());
-//            return true;
-//        }else{
-//            System.out.println("cette transacrtion n'existe pas");
-//            return false;
-//        }
-//    }
-//
-//
-//
-//}
+    public boolean supprimerTransaction(Transaction t, Compte c){
+        if (deja(c,t)){
+            c.getHistoriqueTransactions().remove(t);
+            System.out.println("tu as supprimer le  transacrtion"+t.getIdTransaction());
+            return true;
+        }else{
+            System.out.println("cette transacrtion n'existe pas");
+            return false;
+        }
+    }
+
+
+
+}
