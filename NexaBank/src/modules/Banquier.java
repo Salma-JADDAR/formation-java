22c710e967c0c3b19a43c1add373c0509e7edf7e
Salma GTU <salmajaddar7@gmail.com>
2026-09-11 11:37:49 +0100

    feat(model): define Banquier entity extending Personne

--- NexaBank/src/modules/Banquier.java
+++ NexaBank/src/modules/Banquier.java
@@ -1,20 +1,20 @@
-//package modules;
-//
-//public class Banquier extends Personne{
-//    private  int idBanquier;
-//
-//    public Banquier(String nom,String prenom,String email,String motDepass,char genre,int idBanquier){
-//        super(nom,prenom,email,motDepass,genre);
-//        this.idBanquier=idBanquier;
-//    }
-//
-//    public int getIdBanquier(){
-//        return idBanquier;
-//    }
-//
-//    @Override
-//    public void afficher_Mes_Info_Personnels() {
-//        super.afficher_Mes_Info_Personnels();
-//        System.out.println("eg mon id personnel est :"+getIdBanquier());
-//    }
-//}
+package modules;
+
+public class Banquier extends Personne{
+    private  int idBanquier;
+
+    public Banquier(String nom,String prenom,String email,String motDepass,char genre,int idBanquier){
+        super(nom,prenom,email,motDepass,genre);
+        this.idBanquier=idBanquier;
+    }
+
+    public int getIdBanquier(){
+        return idBanquier;
+    }
+
+    @Override
+    public void afficher_Mes_Info_Personnels() {
+        super.afficher_Mes_Info_Personnels();
+        System.out.println("eg mon id personnel est :"+getIdBanquier());
+    }
+}
