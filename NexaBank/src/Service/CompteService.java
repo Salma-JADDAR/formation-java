4a869088ce6cebfe6165263e6c1f200a17e852ed
Salma GTU <salmajaddar7@gmail.com>
2026-09-14 03:07:29 +0100

    feat(service): implement account statement file persistence

--- NexaBank/src/Service/CompteService.java
+++ NexaBank/src/Service/CompteService.java
@@ -1,7 +1,12 @@
 package Service;
+import Execptions.FichierException;
 import modules.Compte;
 import modules.Transaction;
 import modules.Client;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CompteService {
 
 
@@ -36,6 +41,26 @@ public class CompteService {
         }
     }
 
+
+    public void enregitrerDansUnfichier(Compte cmp)throws FichierException {
+
+            String nomFichier = "compte"+ cmp.getNumcomp()+".txt";
+         try(  FileWriter f= new FileWriter(nomFichier)) {;
+            f.write("Releve Bnacaire de compte"+cmp.getNumcomp()+"\n");
+            for(Transaction t:cmp.getHistoriqueTransactions()){
+                f.write("le numero de transaction est :"+t.getIdTransaction()+"\n");
+                f.write("Transaction de type :"+t.getType()+"\n");
+                f.write("Montant est :"+t.getMontant()+"\n");
+                f.write("est fait le :"+t.getDate()+"\n");
+
+            }
+            f.write("///////////////////////////////////////////");
+            f.close();
+        } catch (IOException e) {
+            throw new FichierException("Erreur lors de l'enregistrement du relevé du compte " + cmp.getNumcomp());
+        }
+
+    }
 
 
 }
